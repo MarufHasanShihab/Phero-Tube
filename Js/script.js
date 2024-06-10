a1503d01f24ee2212472d810d69c5c9e771958c4
Maruf Hasan Shihab <marufhasanshihab2@gmail.com>
2024-06-10 22:40:59 +0600

    aded dynamic card

--- Js/script.js
+++ Js/script.js
@@ -21,11 +21,33 @@ const displayCategory = (categories)=>{
 const loadSingleCategoryData = async(categoryId)=>{
     const res = await fetch(` https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json()
-    console.log(data)
+    displaySingleCategoryData(data.data)
 }
 
 // display single category data
-
+const displaySingleCategoryData = (datas)=>{
+    const categoryCardContainer = document.getElementById("category-card-container");
+    categoryCardContainer.innerHTML = "";
+    datas.forEach(data => {
+        console.log(data)
+        const categoryCard = document.createElement("div");
+        categoryCard.classList = "card card-compact w-[300px] bg-base-100 shadow-xl"
+        categoryCard.innerHTML = `
+        <figure><img class = "h-[200px]" src="${data.thumbnail}"  /></figure>
+        <div class="card-body">
+          <div class="flex gap-4">
+          <img class="w-10 h-10 rounded-full" src="${data.authors[0].profile_picture}">
+          <div>
+          <h2 class="card-title">${data.title}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+          </div>
+        </div>
+        `
+        categoryCardContainer.appendChild(categoryCard)
+    })
+}
 
 
 loadCategory()
+loadSingleCategoryData(1001)
